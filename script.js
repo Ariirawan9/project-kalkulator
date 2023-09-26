558e7b7a3ad109df9d124929e7adbfe6b1451eed
Ariirawan9 <140330214+Ariirawan9@users.noreply.github.com>
2023-09-26 07:41:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator = null;
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleSymbol(value);
+    } else {
+        handleNumber(value);
+    }
+    updateScreen();
+}
+
+function updateScreen() {
+    screen.textContent = buffer;
+}
+
+function handleSymbol(symbol) {
+    switch (symbol) {
+        case 'c':
+            buffer = '0';
+            runningTotal = 0;
+            break;
+        case '=':
+            if (previousOperator === null) {
+                return;
+            }
+            flushOperation(parseFloat(buffer));
+            previousOperator = null;
+            buffer = runningTotal.toString();
+            runningTotal = 0;
+            break;
+            case '←':
+                if (buffer.length === 1) {
+                    buffer = '0';
+                } else {
+                    buffer = buffer.substring(0, buffer.length - 1);
+                }
+                break;
+            case '+':
+            case '-':
+            case 'x':
+            case '÷':
+                handleMath(symbol);
+                break;
+        }
+    }
+
+    function handleMath(symbol) {
+        if (buffer === '0') {
+            return;
+        }
+
+        const floatBuffer = parseFloat(buffer);
+
+        if (runningTotal === 0) {
+            runningTotal = floatBuffer;
+        } else {
+            flushOperation(floatBuffer);
+        }
+        previousOperator = symbol;
+        buffer = '0';
+    }
+    function flushOperation(floatBuffer) {
+        if (previousOperator === '+') {
+            runningTotal += floatBuffer;
+        } else if (previousOperator === '-') {
+            runningTotal -= floatBuffer;
+        } else if (previousOperator === 'x') {
+            runningTotal *= floatBuffer;
+        } else if (previousOperator === '÷') {
+            runningTotal /= floatBuffer;
+        }
+    }
+
+    function handleNumber(numberString) {
+        if (buffer === "0" || buffer === runningTotal.toString()) {
+            buffer = numberString;
+        } else {
+            buffer += numberString;
+        }
+    }
